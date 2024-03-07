3f616bc4af32c4dd648097d282365f203e9f5efc
gongsunyangmei <2484803754@qq.com>
2024-03-07 19:30:13 +0800

    2024-03-07_19:30:13

--- demo/src/vector_add_sim.v
+++ demo/src/vector_add_sim.v
@@ -2,55 +2,54 @@
 
 module core_sim;
     reg clk, rst;
-    wire         io_infor_in_ready,
-    wire         io_infor_in_valid,
-    wire [32:0]  io_infor_in_bits_addr_read,
-    wire [31:0]  io_infor_in_bits_len_read,
-    wire [32:0]  io_infor_in_bits_addr_write,
-    wire [31:0]  io_infor_in_bits_len_write,
-    wire         io_mem_interface_aw_ready,
-    wire         io_mem_interface_aw_valid,
-    wire [32:0]  io_mem_interface_aw_bits_addr, 
-    wire [1:0]   io_mem_interface_aw_bits_burst,
-    wire [3:0]   io_mem_interface_aw_bits_cache,
-    wire [5:0]   io_mem_interface_aw_bits_id,
-    wire [3:0]   io_mem_interface_aw_bits_len,
-    wire         io_mem_interface_aw_bits_lock,
-    wire [2:0]   io_mem_interface_aw_bits_prot,
-    wire [3:0]   io_mem_interface_aw_bits_qos,
-    wire [3:0]   io_mem_interface_aw_bits_region,
-    wire [2:0]   io_mem_interface_aw_bits_size,
-    wire         io_mem_interface_ar_ready,
-    wire         io_mem_interface_ar_valid,
-    wire [32:0]  io_mem_interface_ar_bits_addr,
-    wire [1:0]   io_mem_interface_ar_bits_burst,
-    wire [3:0]   io_mem_interface_ar_bits_cache,
-    wire [5:0]   io_mem_interface_ar_bits_id,
-    wire [3:0]   io_mem_interface_ar_bits_len,
-    wire         io_mem_interface_ar_bits_lock,
-    wire [2:0]   io_mem_interface_ar_bits_prot,
-    wire [3:0]   io_mem_interface_ar_bits_qos,
-    wire [3:0]   io_mem_interface_ar_bits_region,
-    wire [2:0]   io_mem_interface_ar_bits_size,
-    wire         io_mem_interface_w_ready,
-    wire         io_mem_interface_w_valid,
-    wire [255:0] io_mem_interface_w_bits_data,
-    wire         io_mem_interface_w_bits_last,
-    wire [31:0]  io_mem_interface_w_bits_strb,
-    wire         io_mem_interface_r_ready,
-    wire         io_mem_interface_r_valid,
-    wire [255:0] io_mem_interface_r_bits_data,
-    wire         io_mem_interface_r_bits_last,
-    wire [1:0]   io_mem_interface_r_bits_resp,
-    wire [5:0]   io_mem_interface_r_bits_id,
-    wire         io_mem_interface_b_ready,
-    wire         io_mem_interface_b_valid,
-    wire [5:0]   io_mem_interface_b_bits_id,
-    wire [1:0]   io_mem_interface_b_bits_resp,
-    wire         io_infor_out_ready,
-    wire         io_infor_out_valid,
-    wire         io_infor_out_bits,
-  
+    wire         io_infor_in_ready;
+    reg         io_infor_in_valid;
+    reg [32:0]  io_infor_in_bits_addr_read;
+    reg [31:0]  io_infor_in_bits_len_read;
+    reg [32:0]  io_infor_in_bits_addr_write;
+    reg [31:0]  io_infor_in_bits_len_write;
+    reg         io_mem_interface_aw_ready;
+    wire         io_mem_interface_aw_valid;
+    wire [32:0]  io_mem_interface_aw_bits_addr;
+    wire [1:0]   io_mem_interface_aw_bits_burst;
+    wire [3:0]   io_mem_interface_aw_bits_cache;
+    wire [5:0]   io_mem_interface_aw_bits_id;
+    wire [3:0]   io_mem_interface_aw_bits_len;
+    wire         io_mem_interface_aw_bits_lock;
+    wire [2:0]   io_mem_interface_aw_bits_prot;
+    wire [3:0]   io_mem_interface_aw_bits_qos;
+    wire [3:0]   io_mem_interface_aw_bits_region;
+    wire [2:0]   io_mem_interface_aw_bits_size;
+    reg         io_mem_interface_ar_ready;
+    wire         io_mem_interface_ar_valid;
+    wire [32:0]  io_mem_interface_ar_bits_addr;
+    wire [1:0]   io_mem_interface_ar_bits_burst;
+    wire [3:0]   io_mem_interface_ar_bits_cache;
+    wire [5:0]   io_mem_interface_ar_bits_id;
+    wire [3:0]   io_mem_interface_ar_bits_len;
+    wire         io_mem_interface_ar_bits_lock;
+    wire [2:0]   io_mem_interface_ar_bits_prot;
+    wire [3:0]   io_mem_interface_ar_bits_qos;
+    wire [3:0]   io_mem_interface_ar_bits_region;
+    wire [2:0]   io_mem_interface_ar_bits_size;
+    reg         io_mem_interface_w_ready;
+    wire         io_mem_interface_w_valid;
+    wire [255:0] io_mem_interface_w_bits_data;
+    wire         io_mem_interface_w_bits_last;
+    wire [31:0]  io_mem_interface_w_bits_strb;
+    wire         io_mem_interface_r_ready;
+    reg         io_mem_interface_r_valid;
+    reg [255:0] io_mem_interface_r_bits_data;
+    reg         io_mem_interface_r_bits_last;
+    wire [1:0]   io_mem_interface_r_bits_resp;
+    wire [5:0]   io_mem_interface_r_bits_id;
+    wire         io_mem_interface_b_ready;
+    wire         io_mem_interface_b_valid;
+    wire [5:0]   io_mem_interface_b_bits_id;
+    wire [1:0]   io_mem_interface_b_bits_resp;
+    reg          io_infor_out_ready;
+    wire         io_infor_out_valid;
+    wire         io_infor_out_bits;
   vector_add  vector_add(
   .clock(clk),
   .reset(rst),
@@ -101,7 +100,7 @@ module core_sim;
   .io_mem_interface_b_bits_resp(io_mem_interface_b_bits_resp),
   .io_infor_out_ready(io_infor_out_ready),
   .io_infor_out_valid(io_infor_out_valid),
-  .io_infor_out_bit(io_infor_out_bit)
+  .io_infor_out_bits(io_infor_out_bits)
 );
 
     initial begin
@@ -110,10 +109,10 @@ module core_sim;
         #2 rst = 0;
         #2
         io_infor_in_valid = 1;
-        io_infor_out_bits_addr_read = 0;
-        io_infor_out_bits_len_read = 64;
-        io_infor_out_bits_addr_write = 256;
-        io_infor_out_bits_len_write = 64;
+        io_infor_in_bits_addr_read = 0;
+        io_infor_in_bits_len_read = 64;
+        io_infor_in_bits_addr_write = 256;
+        io_infor_in_bits_len_write = 64;
         #2
         io_mem_interface_ar_ready = 1;
         #2
